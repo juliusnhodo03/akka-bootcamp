97d423f3db3ff6cc898c9fe199e53b0240d32431
juliusnhodo03 <ikommaxey@gmail.com>
2023-10-29 17:34:16 +0200

    updates

--- src/Unit-1/DoThis/ValidationActor.cs
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -10,14 +10,14 @@ public class ValidationActor : UntypedActor
     {
         _consoleWriterActor = consoleWriterActor;
     }
+
     protected override void OnReceive(object message)
     {
         var msg = message as string;
         if (string.IsNullOrEmpty(msg))
         {
             // signal that the user needs to supply an input
-            _consoleWriterActor.Tell(
-            new Messages.NullInputError("No input received."));
+            _consoleWriterActor.Tell(new Messages.NullInputError("No input received."));
         }
         else
         {
@@ -33,10 +33,12 @@ public class ValidationActor : UntypedActor
                 _consoleWriterActor.Tell(new Messages.ValidationError("Invalid input had odd number of characters."));
             }
         }
+
         // tell sender to continue doing its thing
         // (whatever that may be, this actor doesn't care)
         Sender.Tell(new Messages.ContinueProcessing());
     }
+
     /// <summary>
     /// Determines if the message received is valid.
     /// Checks if number of chars in message received is even.
